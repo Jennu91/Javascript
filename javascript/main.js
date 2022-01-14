7749e178c685ce459fd4895569f564b56ec35188
Jennu91 <92641005+Jennu91@users.noreply.github.com>
2022-01-14 15:36:54 -0300

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,14 @@
+//fichero pacientes clinicos//
+
+class paciente{
+    Constructor(nombre, edad, domicilio, obraSocial) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.domicilio = domicilio;
+    this.obraSocial= obraSocial;
+}
+}
+
+const paciente1 = new paciente("Mariana", 48 ,"Arregui 543", "Omint");
+const paciente2 = new paciente("Jeronimo", 30, "Nazca 1234", "Galeno");
+const paciente3 = new paciente("Valentin", 30, "Trelles 789", "Osplad");
